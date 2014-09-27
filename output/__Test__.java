b84edb598bbfa8b07a5cf8134e9e9ad15b7d1576
OzuYatamutsu <scollins36@gatech.edu>
2014-09-27 19:51:29 -0400

    Removed direct left-recursion on EXPR and INDEX_EXPR rules (closes #4)

--- output/__Test__.java
+++ output/__Test__.java
@@ -6,12 +6,12 @@ import org.antlr.runtime.debug.DebugEventSocketProxy;
 public class __Test__ {
 
     public static void main(String args[]) throws Exception {
-        tiger lex = new tiger(new ANTLRFileStream("C:\\Users\\Jinhai Steakhouse\\OneDrive\\College\\CS 4240\\tiger-compiler\\output\\__Test___input.txt", "UTF8"));
+        tiger lex = new tiger(new ANTLRFileStream("C:\\Users\\Jinhai Steakhouse\\OneDrive\\College\\CS 4240\\tiger-compiler\\TestCases\\dotproduct.tig", "UTF8"));
         CommonTokenStream tokens = new CommonTokenStream(lex);
 
  g = new (tokens, 49100, null);
         try {
-            g.ID();
+            g.TIGER_PROGRAM();
         } catch (RecognitionException e) {
             e.printStackTrace();
         }
