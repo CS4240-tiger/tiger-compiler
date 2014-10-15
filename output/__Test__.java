c94ae9eb6bb61dde723dc3b060f52be8c9f813e4
OzuYatamutsu <scollins36@gatech.edu>
2014-10-15 12:42:25 -0400

    Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)Attempt to try automated tree walking method via tree grammar (#31)

--- output/__Test__.java
+++ output/__Test__.java
@@ -1,21 +1,31 @@
 import java.io.*;
 
 import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
 import org.antlr.runtime.debug.DebugEventSocketProxy;
-import org.antlr.runtime.tree.CommonTree;
+
 
 public class __Test__ {
 
-    public static void main(String args[]) throws Exception {
+    @SuppressWarnings("unused")
+	public static void main(String args[]) throws Exception {
         tigerLexer lex = new tigerLexer(new ANTLRFileStream("C:\\Users\\Jinhai Steakhouse\\OneDrive\\College\\CS 4240\\tiger-compiler\\TestCases\\testcase.tig", "UTF8"));
         CommonTokenStream tokens = new CommonTokenStream(lex);
 
-        tigerParser g = new tigerParser(tokens);
+
+        tigerParser parser = new tigerParser(tokens);
+        tigerParser.tiger_program_return r = parser.tiger_program();
+        CommonTreeNodeStream nodes = new CommonTreeNodeStream(r.getTree());
+
+
+        tigerTreeWalker walker = new tigerTreeWalker(nodes);
         try {
-            CommonTree ast = g.tiger_program().getTree();
-            Thread.sleep(1); // DEBUG: examine result AST in Debug mode 
+            CommonTree result = walker.tiger_program().getTree();
+            System.nanoTime(); // DEBUG, check result in debug mode
         } catch (RecognitionException e) {
             e.printStackTrace();
         }
+
     }
+
 }
