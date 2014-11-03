67b7620def6f61ab22ceb6a07c4129f4796d37d9
OzuYatamutsu <scollins36@gatech.edu>
2014-11-03 12:39:01 -0500

    Start a static translator class (#28)

---
+++ output/IRGenerator.java
@@ -0,0 +1,43 @@
+/**
+ * An IRGenerator serves to provide direct translations between 
+ * Tiger statements and an intermediate representation.
+ */
+public class IRGenerator {
+
+	// No constructor.
+	
+	/**
+	 * Returns an IR translation of a Tiger function declaration.
+	 * 
+	 * @param tigerInput The input Tiger line.
+	 * @return An IR translation of a Tiger function declaration.
+	 */
+	public static String funct_declaration(String tigerInput) {
+		// TODO: implement
+		return "";
+	}
+	
+	/**
+	 * Returns an IR translation of a Tiger end-block tail.
+	 * 
+	 * @param tigerInput The input Tiger line.
+	 * @return An IR translation of a Tiger end-block tail.
+	 */
+	public static String block_end(String tigerInput) {
+		// TODO: implement
+		return "";
+	}
+	
+	/**
+	 * Returns an IR translation of a Tiger declaration statement.
+	 * 
+	 * @param tigerInput The input Tiger line.
+	 * @return An IR translation of a Tiger declaration statement.
+	 */
+	public static String declaration_statement(String tigerInput) {
+		// TODO: implement
+		return "";
+	}
+	
+	// TODO: Continue from type_declaration downwards
+}
