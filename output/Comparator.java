36538f4fe50487d8d1e61f21be25e4abfbe1b9ef
OzuYatamutsu <scollins36@gatech.edu>
2014-11-03 19:14:11 -0800

    Formatting changes in Comparator

--- output/Comparator.java
+++ output/Comparator.java
@@ -2,28 +2,28 @@
  * A Comparator holds valid binary comparison operators.
  */
 public enum Comparator {
-		/**
-		 * =
-		 */
-		EQUAL,
-		/**
-		 * <>
-		 */
-		NOT_EQUAL,
-		/**
-		 * <
-		 */
-		LESS_THAN,
-		/**
-		 * >
-		 */
-		GREATER_THAN,
-		/**
-		 * <=
-		 */
-		LESS_THAN_OR_EQUAL,
-		/**
-		 * >=
-		 */
-		GREATER_THAN_OR_EQUAL,
+	/**
+	 * =
+	 */
+	EQUAL,
+	/**
+	 * <>
+	 */
+	NOT_EQUAL,
+	/**
+	 * <
+	 */
+	LESS_THAN,
+	/**
+	 * >
+	 */
+	GREATER_THAN,
+	/**
+	 * <=
+	 */
+	LESS_THAN_OR_EQUAL,
+	/**
+	 * >=
+	 */
+	GREATER_THAN_OR_EQUAL
 }
