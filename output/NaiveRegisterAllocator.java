e66273adb8c85fde9727b783f9df7e9e286e2ef3
OzuYatamutsu <scollins36@gatech.edu>
2014-11-25 22:24:31 -0500

    Work on matching temporary allocation per IR line (#76)

--- output/NaiveRegisterAllocator.java
+++ output/NaiveRegisterAllocator.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * A NaiveRegistorAllocator is a class which takes in IR-generated code as 
@@ -11,21 +12,51 @@ public class NaiveRegisterAllocator {
 	 */
 	private List<String> input;
 	/**
-	 * The number of free registers available to allocate.
+	 * Generated opening sections to a MIPS program.
 	 */
-	private int numRegisters;
+	private List<String> mipsPreface;
+	/**
+	 * An array of register numbers to allocate.
+	 */
+	private int[] registers;
 	
 	/**
 	 * Constructs a new NaiveRegisterAllocator with input IR List and number of
 	 * available registers to allocate.
 	 * 
 	 * @param input Input IR lines, as Strings.
-	 * @param numRegisters The number of free registers available to allocate.
+	 * @param numRegisters An array of register numbers to allocate.
 	 */
-	public NaiveRegisterAllocator(List<String> input, int numRegisters) {
+	public NaiveRegisterAllocator(List<String> input, int[] registers) {
 		this.input = input;
-		this.numRegisters = numRegisters;
+		this.registers = registers;
 	}
 	
+	private void storeAllTemporaries() {
+		List<Integer> tempIndexes;
+		mipsPreface.add(".data");
+		
+		for (String line : input) {
+			tempIndexes = findTempInLine(line);
+			if (!tempIndexes.isEmpty()) {
+				for (int index : findTempInLine(line)) {
+					
+				}
+				mipsPreface.add("");
+			}
+		}
+	}
 	
+	private List<Integer> findTempInLine(String input) {
+		String[] lineSplit = input.split("\\s+");
+		List<Integer> indexes = new ArrayList<Integer>();
+		for (int i = 0; i < lineSplit.length; i++) {
+			if (lineSplit[i].matches("[t][0-9]+")) {
+				
+			}
+		}
+		
+		
+		return indexes;
+	}
 }
