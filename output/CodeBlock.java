a6970d344460ed0e54bdc840492f74958f04ea4c
cevanish12 <cevanish12@gmail.com>
2014-12-01 02:02:11 -0500

    added a metric shit ton of local variables for CodeBlock.java that I can hopefully use to make things ezpz

--- output/CodeBlock.java
+++ output/CodeBlock.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class CodeBlock {
@@ -24,11 +26,69 @@ public class CodeBlock {
 	 */
 	private int id;
 	
+	/**
+	 * return type for this block if it is a helper function
+	 */
+	private String returnType;
+	
+	/**
+	 * Map for all the variables in this block and their type
+	 */
+	private Map<String,String> typeMap;
+	
+	/**
+	 * Map for all the integer registers
+	 */
+	private Map<String,String> intRegs;
+	
+	/**
+	 * Map for all the fixedpt registers
+	 */
+	private Map<String,String> fixeptRegs;
+	
+	/**
+	 * All the available integer registers
+	 */
+	private static String[] allIntRegs = new String[]{"$t0", "$t1", "$t2", "$t3", "$t4", 
+		"$t5", "$t6", "$t7", "$t8", "$t9", "$s0", "$s1", 
+		"$s2", "$s3", "$s4", "$s5", "$s6", "$s7"};
+	
+	/**
+	 * All the available fixedpt integers
+	 */
+	private static String[] allFixedPtRegs = new String[]{"$f0", 
+		"$f1", "$f2", "$f3", "$f4", "$f5", "$f6", "$f7", 
+		"$f8", "$f9", "$f10", "$f11", "$f12", "$f13", "$f14", 
+		"$f15", "$f16", "$f17", "$f18", "$f19", "$f20", "$f21", 
+		"$f22", "$f23", "$f24", "$f25", "$f26", "$f27", "$f28", 
+		"$f29", "$f30", "$f31"};
+	
+	/**
+	 * The number of total integer variables
+	 */
+	private int numIntVars;
+	
+	/**
+	 * The number of total fixedpt variables
+	 */
+	private int numFixedPtVars;
+	
+	/**
+	 * @param leader the first line of code of this block
+	 * @param last the last line of code of this block
+	 * @param code the entire code of this block
+	 * @param id the id of this code block
+	 */
 	public CodeBlock(String leader, String last, String[] code, int id) {
 		this.leader= leader;
 		this.last = last;
 		this.code = new ArrayList<String>(Arrays.asList(code));
 		this.id = id;
+		this.typeMap = new HashMap<String,String>();
+		this.intRegs = new HashMap<String,String>();
+		this.fixeptRegs = new HashMap<String,String>();
+		this.numIntVars = 0;
+		this.numFixedPtVars= 0;
 	}
 	
 	public boolean equals(CodeBlock block2) {
